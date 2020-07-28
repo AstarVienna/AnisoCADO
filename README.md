a64974527de95093a6ad66511d5de95c78e89155
Kieran <kdleschinski@gmail.com>
2020-07-28 10:00:07 +0200

    updated setup with smaller licence file

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # AnisoCADO
+
 [![Build Status](https://travis-ci.org/astronomyk/AnisoCADO.svg?branch=master)](https://travis-ci.org/astronomyk/AnisoCADO)
 [![Documentation Status](https://readthedocs.org/projects/anisocado/badge/?version=latest)](https://anisocado.readthedocs.io/en/latest/?badge=latest)
 
-Tested under Python: 2.7, 3.4, 3.5, 3.6, 3.7
+[![Python 2.7](https://img.shields.io/badge/Python-2.7-green.svg)]()
+[![Python >=3.4](https://img.shields.io/badge/Python->=3.4-brightgreen.svg)]()
 
 A python package to generate off-axis PSFs for the SCAO mode for the ELT
 
 Please note: this package is not yet finished yet! The code is fine, but the 
 documentation is lacking.
+
+## Documentation
 
 Apropos documentation. It can be found here:
 [https://anisocado.readthedocs.io/en/latest/index.html](https://anisocado.readthedocs.io/en/latest/index.html)
--- setup.py
+++ setup.py
@@ -19,16 +19,31 @@ with open('LICENSE') as f:
 with open('anisocado/version.py') as f:
     __version__ = f.readline().split("'")[1]
 
-setup(
-    name='anisocado',
-    version=__version__,
-    description='Generate off-axis SCAO PSFs for the ELT',
-    long_description=__readme__,
-    author='Eric Gendron, Kieran Leschinski',
-    author_email='kieran.leschinski@univie.ac.at',
-    url='https://github.com/astronomyk/anisocado',
-    license=__license__,
-    include_package_data=True,
-    packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['numpy', 'astropy', 'matplotlib']
-    )
+
+print(__version__)
+
+
+def setup_package():
+    setup(name='anisocado',
+          version=__version__,
+          description='Generate off-axis SCAO PSFs for the ELT',
+          long_description=__readme__,
+          long_description_content_type="text/markdown",
+          author='Eric Gendron, Kieran Leschinski',
+          author_email='kieran.leschinski@univie.ac.at',
+          url='https://github.com/astronomyk/anisocado',
+          license="GNU general public license",
+          include_package_data=True,
+          packages=find_packages(exclude=('tests', 'docs')),
+          package_dir={'anisocado': 'anisocado'},
+          install_requires=['numpy', 'astropy', 'matplotlib'],
+          classifiers=["Programming Language :: Python :: 3",
+                       "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                       "Operating System :: OS Independent",
+                       "Intended Audience :: Science/Research",
+                       "Topic :: Scientific/Engineering :: Astronomy", ]
+          )
+
+
+if __name__ == '__main__':
+    setup_package()
